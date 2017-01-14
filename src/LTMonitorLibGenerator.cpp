77599771b522107bb1b296fe0a5f7b2fb9cdc11a
Yogi Joshi <joshi.yogi.ramdas@gmail.com>
2017-01-14 16:30:43 -0500

    Fix build issue

--- src/LTMonitorLibGenerator.cpp
+++ src/LTMonitorLibGenerator.cpp
@@ -8,15 +8,25 @@
 
 #include <iostream>
 #include <fstream>
+#include <stdexcept>
 #include "CCodeGenerator.h"
 using namespace std;
 
-int main(int arc, char *argv[]) {
+int main(int argc, char *argv[]) {
 	InputReader ip;
-	ip.parseInputFile();
-	CCodeGenerator cc(ip.mon);
-	cc.writeHeaderCode("1");
-	cc.writeCPPCode("1");
-	cc.writeBaseClass();
+	if ( argc < 2 ){
+		throw invalid_argument("Monitor name not provided");
+	}
+	try {
+		ip.parseInputFile();
+		CCodeGenerator cc(ip.mon);
+		cc.writeHeaderCode(argv[1]);
+		cc.writeCPPCode(argv[1]);
+		cc.writeBaseClass();
+	}
+	catch (ofstream::failure &e) {
+		cerr << "Failed to perform IO ->" << e.what() << endl;
+		return -1;
+	}
 	return 0;
 }
